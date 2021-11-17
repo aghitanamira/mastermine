5934f206487bbb77030433deffa7861a2a359693
aghitanamira <aghitanamira.aghitanamira@gmail.com>
2021-11-18 01:41:02 +0700

    UI login,regis,dashboard

---
+++ app/Models/NoteModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostModel extends Model
+{
+    //sesuai nama tabel
+    protected $table                = 'note';
+    protected $primaryKey           = 'note_id';
+    protected $allowedFields        = ['judul', 'deskripsi', 'kategori', 'slug', 'created_at', 'updated_at'];
+    protected $useTimestamps        = true;
+
+    public function getPosts($slug = false){
+        if($slug == false){
+            return $this->findAll();
+        }
+        
+        return $this->where(['slug' => $slug])->first();
+    }
+
+    
+}
